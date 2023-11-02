eff43663ac65689df30d1d80270c21732ae4369c
athelbiban <vostrovso@gmail.com>
2023-11-02 11:13:01 +0300

    fix: lab2/university/Person.java printAll() + print()

--- src/main/java/j110/lab2/university/Person.java
+++ src/main/java/j110/lab2/university/Person.java
@@ -1,5 +1,7 @@
 package j110.lab2.university;
 
+import j110.lab1.Books.Book;
+
 abstract public class Person {
     private String name;
     private String surname;
@@ -13,41 +15,42 @@ abstract public class Person {
         this.department = department;
     }
 
+//    public void print() {
+//        String printName = name + " " + surname;
+//        String verb = this instanceof Teacher ? "teaches" : "studies";
+//        String pronoun = this.gender.getPronoun();
+//        String pronounPrit = this.gender.getPronounPrit();
+//
+//        System.out.printf("This is %s. %s %s at %s\n",
+//                printName, pronoun, verb, department);
+//
+//        if (this instanceof Teacher) {
+//            System.out.printf("%s has %s degree in %s.\n\n",
+//                    pronoun,
+//                    ((Teacher) this).getDegree(),
+//                    ((Teacher) this).getSpeciality());
+//        }
+//        else if (this instanceof Student) {
+//            System.out.printf("%s is %s'th year %s student.\n\n",
+//                    pronoun,
+//                    ((Student) this).getCourseNumber(),
+//                    ((Student) this).getStage());
+//        }
+//        else {
+//            System.out.printf("%s thesis title is \"%s\"\n\n",
+//                    pronounPrit,
+//                    ((GraduateStudent) this).getThesisTitle());
+//        }
+//    }
+
     public void print() {
-        String printName = name + " " + surname;
-        String verb = this instanceof Teacher ? "teaches" : "studies";
-        String pronoun = this.gender.getPronoun();
-        String pronounPrit = this.gender.getPronounPrit();
 
-        System.out.printf("This is %s. %s %s at %s\n",
-                printName, pronoun, verb, department);
-
-        if (this instanceof Teacher) {
-            System.out.printf("%s has %s degree in %s.\n\n",
-                    pronoun,
-                    ((Teacher) this).getDegree(),
-                    ((Teacher) this).getSpeciality());
-        }
-        else if (this instanceof Student) {
-            System.out.printf("%s is %s'th year %s student.\n\n",
-                    pronoun,
-                    ((Student) this).getCourseNumber(),
-                    ((Student) this).getStage());
-        }
-        else {
-            System.out.printf("%s thesis title is \"%s\"\n\n",
-                    pronounPrit,
-                    ((GraduateStudent) this).getThesisTitle());
-        }
     }
 
     public static void printAll(Person[] persons) {
-        if (persons[0] == null) {
-            System.out.println("Передан пустой массив");
-        } else {
-            for (Person person : persons) {
-                person.print();
-            }
+        Book.dataVerification(persons);
+        for (Person person : persons) {
+            person.print();
         }
     }
 }
